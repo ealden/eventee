f120a1c37f80006e4a669ba29d030b60506763af
Ealden Esto E. Escañan <ealden@escanan.com>
2024-06-19 18:19:26 +0900

    Extract component WeekView

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AllEventsView from './components/AllEventsView.vue'
 import DayView from './components/DayView.vue'
+import WeekView from './components/WeekView.vue'
 import { formatDate, formatTime, formatDateTime, formatHour, formatDateToday, formatMonthYear, formatDay } from './common/format.js'
 import { ref, computed, onMounted } from 'vue'
 import axios from 'axios'
@@ -49,24 +50,6 @@ const groups = computed(() => {
   }
 
   return groups
-})
-
-const week = computed(() => {
-  const startDate = new Date()
-  startDate.setUTCFullYear(today.value.getUTCFullYear())
-  startDate.setUTCMonth(today.value.getUTCMonth())
-  startDate.setUTCDate(today.value.getUTCDate() - today.value.getUTCDay())
-
-  const week = []
-
-  for (let i = 0; i < 7; i++) {
-    const date = new Date(startDate)
-    date.setUTCDate(date.getUTCDate() + i)
-
-    week.push(formatDate(date))
-  }
-
-  return week
 })
 
 const month = computed(() => {
@@ -125,24 +108,6 @@ function weekView() {
 
 function monthView() {
   view.value = 3
-}
-
-function prevWeek() {
-  const date = new Date(today.value)
-  date.setUTCDate(date.getUTCDate() - 7)
-
-  today.value = date
-}
-
-function thisWeek() {
-  today.value = new Date()
-}
-
-function nextWeek() {
-  const date = new Date(today.value)
-  date.setUTCDate(date.getUTCDate() + 7)
-
-  today.value = date
 }
 
 function prevMonth() {
@@ -224,86 +189,7 @@ onMounted(() => {
   </nav>
   <AllEventsView :events="events" :is-current-view="isAllEventsView" />
   <DayView :events="groups" :today="today" :is-current-view="isDayView" />
-  <div id="week-view" v-if="isWeekView">
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          <h2 class="current-period">{{ formatMonthYear(today) }}</h2>
-        </div>
-        <div class="col text-end">
-          <a id="prev-week" href="#" @click="prevWeek">&lt; Prev</a>
-          <a id="this-week" class="mx-1" href="#" @click="thisWeek">Today</a>
-          <a id="next-week" href="#" @click="nextWeek">Next &gt;</a>
-        </div>
-      </div>
-    </div>
-    <div id="week" class="container">
-      <div class="row text-center">
-        <div class="col">
-          &nbsp;
-        </div>
-        <div class="col">
-          <strong>Sun</strong>
-          <span class="day ms-1">
-            <strong>{{ formatDay(week[0]) }}</strong>
-          </span>
-        </div>
-        <div class="col">
-          <strong>Mon</strong>
-          <span class="day ms-1">
-            <strong>{{ formatDay(week[1]) }}</strong>
-          </span>
-        </div>
-        <div class="col">
-          <strong>Tue</strong>
-          <span class="day ms-1">
-            <strong>{{ formatDay(week[2]) }}</strong>
-          </span>
-        </div>
-        <div class="col">
-          <strong>Wed</strong>
-          <span class="day ms-1">
-            <strong>{{ formatDay(week[3]) }}</strong>
-          </span>
-        </div>
-        <div class="col">
-          <strong>Thu</strong>
-          <span class="day ms-1">
-            <strong>{{ formatDay(week[4]) }}</strong>
-          </span>
-        </div>
-        <div class="col">
-          <strong>Fri</strong>
-          <span class="day ms-1">
-            <strong>{{ formatDay(week[5]) }}</strong>
-          </span>
-        </div>
-        <div class="col">
-          <strong>Sat</strong>
-          <span class="day ms-1">
-            <strong>{{ formatDay(week[6]) }}</strong>
-          </span>
-        </div>
-      </div>
-      <div class="row" v-for="hour in 24">
-        <div class="col-1 border text-end">{{ formatHour(hour) }}</div>
-        <div class="col border" v-for="date in week">
-          <div class="row">
-            <div class="col event"
-                 :class="formatDateTime(date, formatHour(hour))"
-                 v-for="event in groups[formatDateTime(date, formatHour(hour))]">
-              <span class="summary">
-                {{ event.summary }}
-              </span>
-              <span class="starts_at visually-hidden">
-                {{ formatTime(event.starts_at) }}
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <WeekView :events="groups" :today="today" :is-current-view="isWeekView" />
   <div id="month-view" v-if="isMonthView">
     <div class="container">
       <div class="row">
---
+++ src/components/WeekView.vue
@@ -0,0 +1,127 @@
+<script setup>
+import { formatDate, formatTime, formatDateTime, formatHour, formatMonthYear, formatDay } from '../common/format.js'
+import { ref, computed } from 'vue'
+
+const props = defineProps(['events', 'today', 'isCurrentView'])
+
+const today = ref(props.today)
+
+const week = computed(() => {
+  const startDate = new Date()
+  startDate.setUTCFullYear(today.value.getUTCFullYear())
+  startDate.setUTCMonth(today.value.getUTCMonth())
+  startDate.setUTCDate(today.value.getUTCDate() - today.value.getUTCDay())
+
+  const week = []
+
+  for (let i = 0; i < 7; i++) {
+    const date = new Date(startDate)
+    date.setUTCDate(date.getUTCDate() + i)
+
+    week.push(formatDate(date))
+  }
+
+  return week
+})
+
+function prevWeek() {
+  const date = new Date(today.value)
+  date.setUTCDate(date.getUTCDate() - 7)
+
+  today.value = date
+}
+
+function thisWeek() {
+  today.value = new Date()
+}
+
+function nextWeek() {
+  const date = new Date(today.value)
+  date.setUTCDate(date.getUTCDate() + 7)
+
+  today.value = date
+}
+</script>
+
+<template>
+  <div id="week-view" v-if="isCurrentView">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h2 class="current-period">{{ formatMonthYear(today) }}</h2>
+        </div>
+        <div class="col text-end">
+          <a id="prev-week" href="#" @click="prevWeek">&lt; Prev</a>
+          <a id="this-week" class="mx-1" href="#" @click="thisWeek">Today</a>
+          <a id="next-week" href="#" @click="nextWeek">Next &gt;</a>
+        </div>
+      </div>
+    </div>
+    <div id="week" class="container">
+      <div class="row text-center">
+        <div class="col">
+          &nbsp;
+        </div>
+        <div class="col">
+          <strong>Sun</strong>
+          <span class="day ms-1">
+            <strong>{{ formatDay(week[0]) }}</strong>
+          </span>
+        </div>
+        <div class="col">
+          <strong>Mon</strong>
+          <span class="day ms-1">
+            <strong>{{ formatDay(week[1]) }}</strong>
+          </span>
+        </div>
+        <div class="col">
+          <strong>Tue</strong>
+          <span class="day ms-1">
+            <strong>{{ formatDay(week[2]) }}</strong>
+          </span>
+        </div>
+        <div class="col">
+          <strong>Wed</strong>
+          <span class="day ms-1">
+            <strong>{{ formatDay(week[3]) }}</strong>
+          </span>
+        </div>
+        <div class="col">
+          <strong>Thu</strong>
+          <span class="day ms-1">
+            <strong>{{ formatDay(week[4]) }}</strong>
+          </span>
+        </div>
+        <div class="col">
+          <strong>Fri</strong>
+          <span class="day ms-1">
+            <strong>{{ formatDay(week[5]) }}</strong>
+          </span>
+        </div>
+        <div class="col">
+          <strong>Sat</strong>
+          <span class="day ms-1">
+            <strong>{{ formatDay(week[6]) }}</strong>
+          </span>
+        </div>
+      </div>
+      <div class="row" v-for="hour in 24">
+        <div class="col-1 border text-end">{{ formatHour(hour) }}</div>
+        <div class="col border" v-for="date in week">
+          <div class="row">
+            <div class="col event"
+                 :class="formatDateTime(date, formatHour(hour))"
+                 v-for="event in events[formatDateTime(date, formatHour(hour))]">
+              <span class="summary">
+                {{ event.summary }}
+              </span>
+              <span class="starts_at visually-hidden">
+                {{ formatTime(event.starts_at) }}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
