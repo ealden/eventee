73e16e78717efffc39b211e954d53f35017268e2
Ealden Esto E. Escañan <ealden@escanan.com>
2024-06-24 16:48:54 +0900

    Style: All Events View

--- app/src/components/AllEventsView.vue
+++ app/src/components/AllEventsView.vue
@@ -6,31 +6,41 @@ defineProps(['events', 'isCurrentView'])
 
 <template>
   <div id="all-events-view" v-if="isCurrentView">
-    <h2 id="currentPeriod">
-      All Events
-    </h2>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Summary</th>
-          <th>Starts At</th>
-          <th>Ends At</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr class="event"
-            v-for="event in events">
-          <td class="summary">
-            {{ event.summary }}
-          </td>
-          <td class="starts_at">
-            {{ formatDateTime(event.starts_at) }}
-          </td>
-          <td class="ends_at">
-            {{ formatDateTime(event.ends_at) }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h2 id="currentPeriod">
+            All Events
+          </h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Event</th>
+                <th>Starts At</th>
+                <th>Ends At</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="event px-1"
+                  v-for="event in events">
+                <td class="summary" width="60%">
+                  {{ event.summary }}
+                </td>
+                <td class="starts_at" width="20%">
+                  {{ formatDateTime(event.starts_at) }}
+                </td>
+                <td class="ends_at" width="20%">
+                  {{ formatDateTime(event.ends_at) }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
