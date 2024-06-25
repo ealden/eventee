2628ecb844e2a1bf0985d9c932be6d919c5040c7
Ealden Esto E. Escañan <ealden@escanan.com>
2024-06-25 12:06:58 +0900

    Year View: Extract calendar section -> YearMonthCalendar.vue

---
+++ app/src/components/YearMonthCalendar.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { computed } from 'vue'
+
+import { formatDay } from '../common/dates.js'
+
+const props = defineProps(['month', 'calendar', 'header'])
+
+const monthKey = computed(() => {
+  return 'month-' + props.month
+})
+</script>
+
+<template>
+  <div class="col px-3">
+    <div class="row">
+      <div class="col fw-bold fs-5 header">
+        {{ header }}
+      </div>
+    </div>
+    <div class="row text-center">
+      <div class="col border"><strong>S</strong></div>
+      <div class="col border"><strong>M</strong></div>
+      <div class="col border"><strong>T</strong></div>
+      <div class="col border"><strong>W</strong></div>
+      <div class="col border"><strong>T</strong></div>
+      <div class="col border"><strong>F</strong></div>
+      <div class="col border"><strong>S</strong></div>
+    </div>
+    <div class="row text-center"
+         v-for="week in calendar">
+      <div class="col border day"
+           :class="monthKey"
+           v-for="day in week">
+        <template v-if="day">
+          {{ formatDay(day) }}
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ app/src/components/__tests__/YearViewCalendar.spec.js
@@ -0,0 +1,27 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import YearMonthCalendar from '../YearMonthCalendar.vue'
+
+describe('YearMonthCalendar', () => {
+  it('renders properly', () => {
+    const wrapper = mount(YearMonthCalendar, {
+      props: {
+        month: 6,
+        calendar: [
+          [ null        , null        , null        , null        , null        , null        , '2024-06-01' ],
+          [ '2024-06-02', '2024-06-03', '2024-06-04', '2024-06-05', '2024-06-06', '2024-06-07', '2024-06-08' ],
+          [ '2024-06-09', '2024-06-10', '2024-06-11', '2024-06-12', '2024-06-13', '2024-06-14', '2024-06-15' ],
+          [ '2024-06-16', '2024-06-17', '2024-06-18', '2024-06-19', '2024-06-20', '2024-06-21', '2024-06-22' ],
+          [ '2024-06-23', '2024-06-24', '2024-06-25', '2024-06-26', '2024-06-27', '2024-06-28', '2024-06-29' ],
+          [ '2024-06-30', null, null, null, null, null, null ]
+        ],
+        header: 'June'
+      }
+    })
+
+    expect(wrapper.get('.header').text()).toEqual('June')
+    expect(wrapper.findAll('.day')).toHaveLength(42)
+    expect(wrapper.findAll('.month-6')).toHaveLength(42)
+  })
+})
