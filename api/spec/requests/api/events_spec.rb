1b332187126bbc732e2c511a2fef291ceda2f307
Ealden Esto E. Escañan <ealden@escanan.com>
2024-07-05 18:52:46 +0900

    Remove 'must'

--- api/spec/requests/api/events_spec.rb
+++ api/spec/requests/api/events_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Api::Events', type: :request do
   describe 'GET /api/events' do
-    it 'must return all events' do
+    it 'returns all events' do
       Event.create summary: 'Event 1', starts_at: '2024-06-14 14:00:00 UTC', ends_at: '2024-06-14 15:00:00 UTC'
       Event.create summary: 'Event 2', starts_at: '2024-06-14 15:00:00 UTC', ends_at: '2024-06-14 16:00:00 UTC'
       Event.create summary: 'Event 3', starts_at: '2024-06-14 17:00:00 UTC', ends_at: '2024-06-14 18:00:00 UTC'
