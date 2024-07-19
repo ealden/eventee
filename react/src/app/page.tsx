fa4cb65ab3fa061d32f34b568853f39f469ec63f
Ealden Esto E. Escañan <ealden@escanan.com>
2024-07-19 12:09:46 +0900

    All Events View: Extract component AllEventsView.tsx

--- react/src/app/page.tsx
+++ react/src/app/page.tsx
@@ -3,14 +3,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-import { formatDateTime } from '../common/dates.js'
-
-interface Event {
-  id: number,
-  summary: string,
-  starts_at: Date,
-  ends_at: Date
-}
+import AllEventsView from '../components/AllEventsView'
 
 export default function App() {
   const [events, setEvents] = useState([])
@@ -71,43 +64,7 @@ export default function App() {
         </div>
       </div>
       <div className="container">
-        <div id="all-events-view">
-          <div className="container mb-1">
-            <div className="row">
-              <div className="col">
-                <h2 id="current-period">
-                  All Events
-                </h2>
-              </div>
-              <div className="col text-end">
-              </div>
-            </div>
-          </div>
-          <table className="table mx-1">
-            <thead>
-              <tr>
-                <th>Event</th>
-                <th>Starts At</th>
-                <th>Ends At</th>
-              </tr>
-            </thead>
-            <tbody>
-            {events.map((event: Event) => (
-              <tr className="event px-1" key={event.id}>
-                <td className="summary" width="60%">
-                  {event.summary}
-                </td>
-                <td className="starts_at" width="20%">
-                  {formatDateTime(event.starts_at)}
-                </td>
-                <td className="ends_at" width="20%">
-                  {formatDateTime(event.ends_at)}
-                </td>
-              </tr>
-            ))}
-            </tbody>
-          </table>
-        </div>
+        <AllEventsView events={events} />
       </div>
     </>
   )
---
+++ react/src/components/AllEventsView.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { formatDateTime } from '../common/dates.js'
+
+interface Event {
+  id: number,
+  summary: string,
+  starts_at: Date,
+  ends_at: Date
+}
+
+export default function AllEventsView({
+  events
+}: Readonly<{
+  events: Event[]
+}>) {
+  return (
+    <div id="all-events-view">
+      <div className="container mb-1">
+        <div className="row">
+          <div className="col">
+            <h2 id="current-period">
+              All Events
+            </h2>
+          </div>
+          <div className="col text-end">
+          </div>
+        </div>
+      </div>
+      <table className="table mx-1">
+        <thead>
+          <tr>
+            <th>Event</th>
+            <th>Starts At</th>
+            <th>Ends At</th>
+          </tr>
+        </thead>
+        <tbody>
+        {events.map((event) => (
+          <tr className="event px-1" key={event.id}>
+            <td className="summary" width="60%">
+              {event.summary}
+            </td>
+            <td className="starts_at" width="20%">
+              {formatDateTime(event.starts_at)}
+            </td>
+            <td className="ends_at" width="20%">
+              {formatDateTime(event.ends_at)}
+            </td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
